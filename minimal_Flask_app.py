e378554c3145117d41b3dc0fc2f3d303c36c8c4d
Mollie <mary.breenlyles@gmail.com>
2019-04-29 01:21:38 -0500

    Created initial bash and WSGI app for RoundTable
    Beginning steps to building RoundTable, see Notes

---
+++ minimal_Flask_app.py
@@ -0,0 +1,22 @@
+from flask import Flask     # > Import Flask class
+app = Flask(__name__)       # > Create *instance* of Flask Class obj 
+                            #   - (will be our WSGI application)*
+                         
+
+@app.route('/')             # > Uses route() decorator to tell Flask
+                            #   what URL should trigger our func
+def hello_world():          # > Name our function (also used to generate URL's for 
+                            #   this particular function)
+    return 'Hello, World!'  # > Return message we want to display on User's browser
+
+
+
+''' * --  1st arg is name of app's module/package
+          We use single module, so we use __name__ b/c
+          __name__ is different depending on if started
+          as app or imported as module. '__main__' for app
+          vs. actual import name. (Flask needs name to know
+          where to look for tmeplates, static files, etc.
+          See Flask documentation for more.
+
+'''
